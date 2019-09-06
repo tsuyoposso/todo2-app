7bd83202482a216790b218ecab5359a094ead7fc
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-09-06 11:31:14 +0900

    first commit

---
+++ todo2-app/todo2-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo2-app
+//
+//  Created by 長坂豪士 on 2019/09/06.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
